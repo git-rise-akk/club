cb536c9ee13a7186572cc0b958b9be0073d75827
Aleksandr Tkachev <atkachev@art3d.ru>
2025-08-13 02:55:17 +0300

    deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,5 @@
 import { defineConfig } from 'vite';
-import path from "path";
 
 export default defineConfig({
-  // base: '/club/',
-  // css: {
-  //   preprocessorOptions: {
-  //     scss: {
-  //       additionalData: `@use "${path
-  //         .resolve(__dirname, 'src/style/mixins.scss')
-  //         .replace(/\\/g, '/')}" as *;`
-  //     }
-  //   }
-  // }
+  base: '/club/',
 });
